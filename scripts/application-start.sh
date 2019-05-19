825f052d315dacdd912e14d4f120ea8648aa8d25
yanhe <yanhe@didichuxing.com>
2019-05-18 17:35:03 -0700

    add all back

---
+++ scripts/application-start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /tmp/CodeDeployExample
+
+#python3 HTTPServerPY3.py > application-start.txt
+
+echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
